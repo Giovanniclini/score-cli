304e0aa3c68133b244e12007f55e56db57211571
Giovanni Clini <gioviclini@gmail.com>
2025-07-23 12:43:55 +0200

    Refined project structure and added test for Command::parse

--- src/commands.rs
+++ src/commands.rs
@@ -1,19 +1,59 @@
 use crate::commands::add_player::AddPlayer;
 
 mod models;
-pub mod add_player;
+mod utils;
+mod add_player;
 
 #[derive(Debug)]
 pub enum Command<'a> {
-    AddPlayer(AddPlayer),
+    AddPlayer(Vec<String>),
     Invalid(&'a str)
 }
 
 impl<'a> Command<'a> {
     pub fn run(&self) -> Result<(), &'a str> {
         match &self {
-            Command::AddPlayer(cmd) => cmd.run(),
-            Command::Invalid(err) => Err(err)
+            Command::AddPlayer(args) => {
+                let command = AddPlayer::parse(&args)?;
+                command.run()
+            },
+            Command::Invalid(err) => {
+                Err(err)
+            }
+        }
+    }
+
+    pub fn parse(args: &[String]) -> Command {
+        match args.get(0).map(|s| s.as_str()) {
+            Some("add-player") => Command::AddPlayer(args[1..].to_vec()),
+            _ => Command::Invalid("Unknown or missing command.")
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_add_player() {
+        let args = vec![String::from("add-player"), String::from("player-name")];
+        let command = Command::parse(&args);
+        if let Command::AddPlayer(inner_args) = command {
+            assert_eq!(inner_args, vec!["player-name"]);
+        } else {
+            panic!("Expected Command::AddPlayer variant");
+        }
+    }
+
+    #[test]
+    fn parse_invalid() {
+        let args = vec![String::from("invalid-command"), String::from("player-name")];
+        let command = Command::parse(&args);
+        if let Command::Invalid(err) = command {
+            assert_eq!(err, "Unknown or missing command.");
+        } else {
+            panic!("Expected Command::Invalid variant");
         }
     }
 }
